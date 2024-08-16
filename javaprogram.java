3c2d2ba1f2e94277bc90db6870284b8f71288689
kirthigithubit <kiruthikaraju7@gmail.com>
2024-08-16 10:46:47 +0530

    add file

--- javaprogram.java
+++ javaprogram.java
@@ -1,11 +1,12 @@
 public class javaprogram {
 
 	public static void main(String[] args) {
-		
+              System.out.println(("calculator"));	
        int a=10;
        int b=10;
        int c=a+b;
        System.out.println(c);
+
 	}
 
 }
